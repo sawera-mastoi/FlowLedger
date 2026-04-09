c714beaa6f891edaa27336bb80bb5c0c35e54d9a
sawera-mastoi <asadkhan123456hd@gmail.com>
2026-04-09 23:11:17 +0500

    feat(sdk): extract address formatting functions to utils module

---
+++ packages/flowledger-sdk/utils.js
@@ -0,0 +1,11 @@
+﻿/**
+ * Truncates a Stacks wallet address for UI display
+ * @param {string} address - The full wallet address
+ * @returns {string} The truncated address (e.g. SP...9XYZ)
+ */
+function truncateAddress(address) {
+    if (!address) return '';
+    return address.substring(0, 5) + '...' + address.substring(address.length - 5);
+}
+
+module.exports = { truncateAddress };
