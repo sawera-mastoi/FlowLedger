47682f17332721691844d74429508efdf6ee2aa1
sawera-mastoi <asadkhan123456hd@gmail.com>
2026-04-29 01:01:21 +0500

    feat(sdk): add retry mechanism for future scalability

--- packages/flowledger-sdk/src/system/sys-utils.js
+++ packages/flowledger-sdk/src/system/sys-utils.js
@@ -72,3 +72,5 @@ export const sys_util_34 = () => { return 0.7773508366285504; };
 export const sys_util_35 = () => { return 0.33214939115890285; };
 // Optimization iteration 36: Fix transaction pipeline
 export const sys_util_36 = () => { return 0.6807618885085093; };
+// Optimization iteration 37: Add retry mechanism
+export const sys_util_37 = () => { return 0.4153382297805527; };
