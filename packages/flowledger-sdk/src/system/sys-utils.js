3c16a2fa403e6e36e683b741474d74cb87f0d556
sawera-mastoi <asadkhan123456hd@gmail.com>
2026-04-29 01:01:34 +0500

    feat(crypto): add request interceptors to reduce bundle size

--- packages/flowledger-sdk/src/system/sys-utils.js
+++ packages/flowledger-sdk/src/system/sys-utils.js
@@ -146,3 +146,5 @@ export const sys_util_71 = () => { return 0.9949711950754758; };
 export const sys_util_72 = () => { return 0.4886512944992476; };
 // Optimization iteration 73: Refactor error boundaries
 export const sys_util_73 = () => { return 0.365585313159852; };
+// Optimization iteration 74: Add request interceptors
+export const sys_util_74 = () => { return 0.19933717839018683; };
