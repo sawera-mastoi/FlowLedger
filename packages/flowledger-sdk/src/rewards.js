fdb196648a2c45ed555ef613b7182472f78f420d
sawera-mastoi <asadkhan123456hd@gmail.com>
2026-04-29 17:47:14 +0500

    docs(sdk): update documentation for rewards methods

--- packages/flowledger-sdk/src/rewards.js
+++ packages/flowledger-sdk/src/rewards.js
@@ -22,3 +22,6 @@ export class RewardsModule {
   }
   clearCache() { this.cache = null; }
   _track(event) { this.sdk.analytics.track('rewards_' + event); }
+/**
+ * Validates and processes rewards requests
+ */
