1169b2adfe46ae206c0988d380867388ebd194f2
sawera-mastoi <asadkhan123456hd@gmail.com>
2026-04-29 17:49:49 +0500

    feat(sdk): add validation helpers for vault inputs

--- packages/flowledger-sdk/src/vault.js
+++ packages/flowledger-sdk/src/vault.js
@@ -16,3 +16,7 @@ export class VaultModule {
   _handleError(err) {
     throw new Error(`[vault] Operation failed: ${err.message}`);
   }
+  validate(input) {
+    if (!input) this._handleError({ message: 'Input required' });
+    return true;
+  }
